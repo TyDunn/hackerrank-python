a18589bb83a878a9a1d3b8700849d600560d5494
tydunn <tydunn@umich.edu>
2018-04-26 15:46:06 -0400

    updating tuples

--- Python/Basic_Data_Types/tuples.py
+++ Python/Basic_Data_Types/tuples.py
@@ -1,5 +1,4 @@
 if __name__ == '__main__':
     n = int(input())
     integer_list = map(int, input().split())
-    input_list = [int(x) for x in integer_list]
-    print(hash(tuple(input_list)))
+    print(hash(tuple(integer_list)))
