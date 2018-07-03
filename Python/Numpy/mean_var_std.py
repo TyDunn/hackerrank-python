229b7135890260e0407d5a361edf4a4ef5c04de7
tydunn <tydunn@umich.edu>
2018-07-03 07:02:24 -0400

    mean, variance, and std deviation

---
+++ Python/Numpy/mean_var_std.py
@@ -0,0 +1,10 @@
+import numpy
+numpy.set_printoptions(legacy='1.13')
+
+n, m = map(int, input().split())
+
+arr = numpy.array([input().split() for _ in range(n)], int)
+
+print(numpy.mean(arr, axis=1)) 
+print(numpy.var(arr, axis=0))
+print(numpy.std(arr, axis=None))
