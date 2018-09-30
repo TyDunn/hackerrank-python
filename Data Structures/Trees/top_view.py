5945b98a59e8d5f62e0a28817abe516bccff2af1
tydunn <tydunn@umich.edu>
2018-09-30 15:52:54 -0400

    Print top view

---
+++ Data Structures/Trees/top_view.py
@@ -0,0 +1,62 @@
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+def topView(root):
+    leftView(root.left)
+    print(root.info, end= " ")
+    rightView(root.right)
+    
+def leftView(root):
+    if not root: 
+        return
+    leftView(root.left)
+    print(root.info, end= " ")
+    
+def rightView(root):
+    if not root:
+        return
+    print(root.info, end= " ")
+    rightView(root.right)
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+topView(tree.root)
