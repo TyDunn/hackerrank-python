2b1db41a1382a5b7d37c51dfec0205835d1e1c78
tydunn <tydunn@umich.edu>
2018-10-03 07:28:04 -0400

    Stack

---
+++ Data Structures/Stack/max_ele.py
@@ -0,0 +1,9 @@
+stack = [0]
+for _ in range(int(input())):
+    query = list(map(int, input().split()))
+    if query[0] == 1:
+        stack.append(max(query[1], stack[-1]))
+    elif query[0] == 2:
+        stack.pop()
+    else:
+        print(stack[-1])
