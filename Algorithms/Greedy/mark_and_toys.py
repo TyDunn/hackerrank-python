0fcaf64b613e27f26826c6d2f5c5f8fff7565166
tydunn <tydunn@umich.edu>
2018-10-27 17:30:25 -0400

    Mark and Toys

---
+++ Algorithms/Greedy/mark_and_toys.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the maximumToys function below.
+def maximumToys(prices, k):
+    prices, num, idx = sorted(prices), 0, 0
+    while k - prices[idx] > 0:
+        num += 1
+        k = k - prices[idx]
+        idx += 1
+    return num
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    prices = list(map(int, input().rstrip().split()))
+
+    result = maximumToys(prices, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
