1cc6a827cfe437e3e551e949344470581d85c7f8
tydunn <tydunn@umich.edu>
2019-02-21 05:58:10 -0500

    shuffle array

---
+++ GfG/Random/shuffle_array.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+import random
+
+def randomize(arr, n):
+	for i in range(n-1, 0, -1):
+		j = random.randint(0, i+1)
+		arr[i], arr[j] = arr[j], arr[i]
+	return arr
+
+if __name__ == '__main__':
+	arr = [1, 2, 3, 4, 5, 6, 7, 8]
+	n = len(arr)
+	print(randomize(arr, n))
